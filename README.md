e1abb47b34b1ce96e46328655fe0c0ce4b6be9e6
Sean Ryan Chan <seanryachan@gmail.coml>
2018-05-30 01:37:10 +0800

    v2.0 It is now finished.

--- README.md
+++ README.md
@@ -1,38 +1,38 @@
+ //separate textfile
+CS 140 SQRUV MP Group
+Azcarraga, Jose Carlos Rodrigo J.   201508037   SQRUV
+Chan, Sean Ryan S.                  201600012   SQRUV
+Kopio, Katrina Mae D.               201601510   SQRUV
+
 This is the readme for the 140 ME
 
 Game Specs:
 
-The game is called: GALAGA iPens
-
-The goal of the game is to destroy the enemy bug
-
-by using the specialized phallic shaped bullets.
+The game is called: GALAGA iPens made by the ACK group. The goal of the game is to destroy the enemy swarm, composed of bugs. The user will be able to control the ship with their arrow keys, and shoot out bullets to defend against the swarm.
 
 
+Playing Instructions:
+1. The player has a ship that can move from left, right, up down by pressing the corresponding arrow keys (LEFT,RIGHT,UP,DOWN).
 
-The player has a ship that can move from left, right, up down by pressing the corresponding arrow keys (LEFT,RIGHT,UP,DOWN).
+2. The enemy has a swarm of bugs, and it shifts from left to right and goes down every time it goes to the end of each line. When the enemy line has forwarded to the lower end of the screen, the user loses.
 
+3. To fire the special bullet, press 'F'.
 
+4. Every time the bullet hits a bug, that bug dies. Once all bugs are dead, the player will win.
 
-To fire the special bullet, press 'F'.
 
 
 Running the Game:
+
+Environment Specs:
+    -Linux system
+    -Linux posix threads library installed
+    -Linux ncurses library installed
+
+Execution Instructions
 1. Make sure ncurses and pthreads is installed
 2. In terminal open the game folder
-3. Then to fix permissions type
+3. Then fix permissions by typing:
     chmod +x runMP.sh
-3. to open the game type
+3. To open the game type:
     ./runMP.sh
-
-
-
-
-Tasks natin:
-    1) text files with names
-    2) readme:
-        -how to run program
-        -third party library
-        -program limitations
-        -environment specs and restrictions
-    3) c code
--- mp_main.c
+++ mp_main.c
@@ -6,47 +6,71 @@
 #include <ncurses.h>
 #include <unistd.h>
 
-/*
-   atom://teletype/portal/41eac518-2e39-4494-985b-3b92f82d20a9
- */
-
+//DEFINITIONS
 #define MAX_THREADS 2048
 #define SHIPRATE 50000
-#define BULLETRATE 75000
+#define BULLETRATE 100000 // goes up the screen 10 times a second.
 #define REFRESH_RATE 10000 // refreshes screens 100 times a second, via doupdate()
+#define BUGRATE 100000
 #define DELAY 50000
 #define BUGS_ROW 4
 #define BUGS_COL 8
-#define BUG_ONE "."
-#define BUG_TWO "o"
+#define LEFT -1
+#define RIGHT 1
+#define ALIVE 1
+#define DEAD 0
+#define WIN 1
+#define LOSE 0
+#define RUNNING -1
 
+
+//DECLARATIONS
 pthread_mutex_t gameLock;
 pthread_mutex_t ex;
 
 typedef struct Node {
-        int bugx;
-        int bugy;
-        int status;
-        struct Node *next;
+    int bugx;
+    int bugy;
+    int status;
+    struct Node *next;
+    char display;
 } Node;
 
 typedef struct windowData {//data type that stores all the game data
-        int bulX, bulY;
-        int shipX, shipY;
-        Node bugs[10];//we need to declare their initial coordinates
-        int maxX;
-        int maxY;
-        int bulletThread; // for generation of multiple bullets
-        pthread_t threads[MAX_THREADS];
-        // This contains the threads of the game. Threads 10 to 2047 are reserved for bullets.
-        WINDOW *scr; // points to the window
+    int bulX, bulY;
+    int shipX, shipY;
+    Node bugs[10];//we need to declare their initial coordinates
+    int directionBug;
+    int maxX;
+    int maxY;
+    int bulletThread; // for generation of multiple bullets
+    pthread_t threads[MAX_THREADS];
+    // This contains the threads of the game. Threads 10 to 2047 are reserved for bullets.
+    WINDOW *scr; // points to the window
+    int bugCount;
+    int winState;
 } windowData;
 
 void drawShip(windowData *game);
 void drawBullet(int bulY, int bulX);
-void fireBullet(windowData *game);
-void shipSpawn(windowData *game); //NOT USED
+void drawBug(windowData *game);
 
+void eraseSingle(int Y, int X);
+void eraseShip(int Y, int X);
+void eraseBullet(int Y, int X);
+void eraseBugs(windowData *game);
+
+void moveBug(windowData *game);
+int killBug(windowData *game, int bulY, int bulX);
+
+void shipThread(windowData *game);
+void swarmThread(windowData *game);
+void screenThread(windowData *game);
+void fireBullet(windowData *game);
+
+void loseCondition(windowData *game);
+
+// ASCII DRAWINGS
 // Ship is drawn top to bottom.
 char *shipHead = " ^ ";
 char *shipBody = "{O}";
@@ -54,13 +78,11 @@ char *shipBody = "{O}";
 char *bulletHead = " ^ ";
 char *bulletBody = " | ";
 char *bulletTail = "O O";
-// P.S. this is my actual size x100
-
-
 // bugHead
-char *bugHead = "O~";
-//
+char *bugHead = "@";
 
+char *replace = " ";
+//START OF CODE
 int main(int argc, char const *argv[]) {
     // setlocale(LC_ALL, **);
     initscr();
@@ -73,8 +95,8 @@ int main(int argc, char const *argv[]) {
     pthread_mutex_init(&gameLock, NULL);
     pthread_mutex_init(&ex, NULL);
 
-//GAME DECLARATION
-    windowData game; //game is the variable that has the ships location, the bullets location
+    //GAME DECLARATION
+    windowData game; //game is the variable that has the ship's location, the bug's location
     getmaxyx(stdscr, game.maxY, game.maxX);
     game.scr = stdscr;
     game.bulletThread = 1; // threads 10 to MAX_THREADS - 1 are for bullets
@@ -83,42 +105,100 @@ int main(int argc, char const *argv[]) {
     game.bulY = -5;
     game.bulX = -5;
     game.shipX = game.maxX / 2;
-    game.shipY = game.maxY / 2;
+    game.shipY = game.maxY - 3;
 
     // initial swarm coordinate declaration
+    game.directionBug = RIGHT;
+    game.bugCount = 10;
+
+    // GAME STATE
+    game.winState = RUNNING;
+
+    // BUG SWARM POPULATION
     for(int i = 0; i < 10; i++) {
-        game.bugs[i].bugx = i;
-        game.bugs[i].bugy = 0;
-        game.bugs[i].status = 1; //all alive
+        game.bugs[i].bugx = i * 3;
+        game.bugs[i].bugy = 1;
+        game.bugs[i].display = '@';
+        game.bugs[i].status = ALIVE; //all alive
     }
 
-    pthread_create(&(game.threads[0]), NULL, (void *)shipSpawn, (void *)&game);
-    // Ship is spawned and controlled by thread 0.
+    pthread_create(&(game.threads[0]), NULL, (void *)shipThread, (void *)&game);
+
+    //create swarm -> swarm is spawned and controlled by thread 1.
+    pthread_create(&(game.threads[1]), NULL, (void *)swarmThread, (void *)&game);
+
+    pthread_create(&(game.threads[2]), NULL, (void *)screenThread, (void *)&game);
 
     int ch;
     while(true) {
+        // win evaluator
+        if(game.bugCount <= 0)
+            game.winState = WIN;
+
+        loseCondition(&game);
+
+        switch (game.winState) {
+            case WIN:
+                erase();
+                while(true){
+                    mvprintw(game.maxY/2,game.maxX/2, "You won!");
+                    refresh();
+                }
+                break;
+            case LOSE:
+                erase();
+                while(true){
+                    mvprintw(game.maxY/2,game.maxX/2, "You have lost the game!");
+                    mvprintw((game.maxY/2 )+ 1,game.maxX/2, "Try again!");
+                    refresh();
+                }
+                break;
+        }
+
         ch = getch();
-        if('f' == ch) {
+
+        if('f' == ch) {// The character to fire a bullet
+            pthread_mutex_lock(&gameLock);
             game.bulX = game.shipX;
             game.bulY = game.shipY-3;
+            // bullet threads -> handled by threads 10 to 2047.
             pthread_create(&(game.threads[game.bulletThread]), NULL, (void *)fireBullet, (void *)&game);
-            if (game.bulletThread == (MAX_THREADS - 1))
+            pthread_mutex_unlock(&gameLock);
+            if (game.bulletThread == (MAX_THREADS - 1)){
+                pthread_mutex_lock(&gameLock);
                 game.bulletThread = 10;
-            else
+                pthread_mutex_unlock(&gameLock);
+            } else{
+                pthread_mutex_lock(&gameLock);
                 game.bulletThread += 1;
+                pthread_mutex_unlock(&gameLock);
+            }
         } else if (KEY_UP == ch) {
-            if(game.shipY > 0)
-                    game.shipY -=1;
+            if(game.shipY > 0){
+                pthread_mutex_lock(&gameLock);
+                game.shipY -=1;
+                pthread_mutex_unlock(&gameLock);
+            }
         } else if (KEY_DOWN == ch) {
-            if(game.shipY < (game.maxY - 2))
-                    game.shipY +=1;
+            if(game.shipY < (game.maxY - 2)){
+                pthread_mutex_lock(&gameLock);
+                game.shipY +=1;
+                pthread_mutex_unlock(&gameLock);
+            }
         } else if (KEY_LEFT == ch) {
-            if(game.shipX > 0)
+            if(game.shipX > 0){
+                pthread_mutex_lock(&gameLock);
                 game.shipX -=1;
+                pthread_mutex_unlock(&gameLock);
+            }
         } else if (KEY_RIGHT == ch) {
-            if(game.shipX < game.maxX - 3)
+            if(game.shipX < game.maxX - 3){
+                pthread_mutex_lock(&gameLock);
                 game.shipX +=1;
+                pthread_mutex_unlock(&gameLock);
+            }
         }
+
         ch = '\0';
     }
 
@@ -126,12 +206,39 @@ int main(int argc, char const *argv[]) {
     return 0;
 }
 
-void keyboardListener(windowData *game){
-    int ch;
+void shipThread(windowData *game){
+    int oldY;
+    int oldX;
+    while(true){
 
+        pthread_mutex_lock(&gameLock);
+        drawShip(game);
+        oldY = game->shipY;
+        oldX = game->shipX;
+        pthread_mutex_unlock(&gameLock);
+
+        usleep(REFRESH_RATE);
+
+        pthread_mutex_lock(&gameLock);
+        eraseShip(oldY, oldX);
+        pthread_mutex_unlock(&gameLock);
+
+        switch (game->winState){
+            case WIN:
+                eraseShip(oldY, oldX);
+                pthread_exit((void *)0);
+                return;
+            case LOSE:
+                eraseShip(oldY, oldX);
+                pthread_exit((void *)0);
+                return;
+
+        }
+
+    }
+    pthread_exit((void*) 0);
 }
 
-//draws ship by
 void drawShip(windowData *game){
     mvprintw(game->shipY, game->shipX, shipHead);
     mvprintw(game->shipY +1, game->shipX, shipBody);
@@ -143,142 +250,167 @@ void drawBullet(int bulY, int bulX){
     mvprintw(bulY + 2, bulX, bulletTail);
 }
 
-void drawBug(windowData *game) {
-    for(int bug_num = 0; bug_num < 10; bug_num++) {
-        if(game->bugs[bug_num].status == 1) {
-            mvprintw(game->bugs[bug_num].bugx, game->bugs[bug_num].bugy, bugHead);
-        }
-    }
-/* FOR PRINTING
-    for bug_number < 10
-        if bugs[bug_number].status == 1
-            print (character "*", bugs[bug_number].bugx , bugs[bug_number].bugy)
-    */
-
-        /* int y = currentY;
-           int x = currentX;
-
-           for (int i = 0; i < BUG_ROW; i++) {
-            for (int j = 0; j < BUG_COL; j++) {
-                if (swarm[i][j]) {
-                    mvprintw(currentY+i, currentX+j, BUG_ONE);
-                }
-                //if(fertilized), then print BUG_TWO
-            }
-            printf("\n");
-           }*/
-}
-
-void shipSpawn(windowData *game){
-    while(true) {
-        pthread_mutex_lock(&gameLock);
-        drawShip(game);
-        // drawBullet(game);
-        refresh();
-        pthread_mutex_unlock(&gameLock);
-
-        usleep(SHIPRATE);
-
-        pthread_mutex_lock(&gameLock);
-        erase();
-        pthread_mutex_unlock(&gameLock);
-
-        pthread_join(game->threads[game->bulletThread], (void *) 0);
-        // This thread is stopped temporarily if there is a bullet fired.
-    }
-    pthread_exit((void *) 0);
-}
-
 void fireBullet(windowData *game){
     int err;
     int bulX = game->shipX;
     int bulY = game->shipY - 3;
-    int 
-    while(true) {
+    int bulmaxY = -3;
+    pthread_join(game->threads[2], (void *) 0);
+    // join all bullets with the ship thread.
+    int deadbug = 0;
+    while(true){
         pthread_mutex_lock(&gameLock);
         drawBullet(bulY, bulX);
-        drawShip(game);
-        refresh();
+        deadbug = killBug(game, bulY, bulX);
         pthread_mutex_unlock(&gameLock);
 
-        usleep(SHIPRATE);
+        usleep(BULLETRATE);
 
         pthread_mutex_lock(&gameLock);
-        erase();
+        eraseBullet(bulY, bulX);
         pthread_mutex_unlock(&gameLock);
 
+        if(bulY <= bulmaxY){
+            pthread_mutex_lock(&gameLock);
+            eraseBullet(bulY, bulX);
+            pthread_mutex_unlock(&gameLock);
+            pthread_exit((void *) 0);
+            return;
+        }
+        if(deadbug){
+            pthread_mutex_lock(&gameLock);
+            eraseBullet(bulY, bulX);
+            pthread_mutex_unlock(&gameLock);
+            pthread_exit((void *) 0);
+            return;
+        }
+        if(game->winState == WIN || game->winState == LOSE){
+            eraseBullet(bulY, bulX);
+            break;
+        }
         bulY -= 1;
-
-        if (bulY <= -3)
-                break;
     }
-
-    // game -> bulY = -5;
-    // game -> bulX = -5;
     pthread_exit((void *) 0);
+    return;
 }
 
 void swarmThread(windowData *game) {
+    // pthread_join(game->threads[2], (void *) 0);
 
     while(true) {
         pthread_mutex_lock(&gameLock);
         drawBug(game);
-        //moveBug(game);
-        //killBug(game);
-        refresh();
         pthread_mutex_unlock(&gameLock);
-
-        usleep(SHIPRATE);
-
+        usleep(BUGRATE);
         pthread_mutex_lock(&gameLock);
-        erase();
+        eraseBugs(game);
+        moveBug(game);
         pthread_mutex_unlock(&gameLock);
 
-        //pthread_join(game->threads[game->bulletThread], (void *) 0);
-        // This thread is stopped temporarily if there is a bullet fired.
+        if(game->winState == WIN || game->winState == LOSE){
+            erase();
+            break;
+        }
     }
     pthread_exit((void *) 0);
 }
 
-/*
-    game pseudocode
+void drawBug(windowData *game) {
+    int bugCountTemp = 0;
+    for(int number = 0; number < 10; number++) {
+        if(game->bugs[number].status == ALIVE) {
+            bugCountTemp++;
+            mvprintw(game->bugs[number].bugy, game->bugs[number].bugx, &(game->bugs[number].display));
+        }
+    }
+    game->bugCount = bugCountTemp;
+    //pwede ba to kasi
+}
 
-    there are 10 bugs in one line
+void moveBug(windowData *game) {
 
- **********
+    for(int i = 0; i < 10; i++){
+        if(game->directionBug == RIGHT){ // move to the right
+            game->bugs[i].bugx= game->bugs[i].bugx + 1;
+        }
+        else if(game->directionBug == LEFT){ // move to the left
+            game->bugs[i].bugx= game->bugs[i].bugx - 1;
+        }
 
-    //drawBug();
-    //moveBug()
-    //killBug();
+        //checking if it is in the end
+        if((game->bugs[i].bugx == game->maxX || game->bugs[i].bugx == 0)&&(game->bugs[i].status == ALIVE)){
+            game->directionBug = game->directionBug*(-1);
+            //when changing direction, it goes down
+            for(int j = 0 ; j < 10; j++){
+                game->bugs[j].bugy = game->bugs[j].bugy + 1;
+            }
+        }
+    }
+}
 
-    for simple movement of each bug
-        move to right:
-        for i < 10
-            bugs[i].bugx = bugs[i].bugx + 1
+int killBug(windowData *game, int bulY, int bulX) {
+    for(int i = 0; i < 10; i++) {
+        if((game->bugs[i].bugx == bulX) && (game->bugs[i].bugy >= bulY)){
+            game->bugs[i].status = DEAD; //bug has died x_x
+            game->bugs[i].display = '\0';
+            return 1; //bullet collided with bug
+        }
+    }
+    return 0;
+}
 
-        move to left:
-        for i < 10
-            bugs[i].bugx = bugs[i].bugx - 1
-
-        move up:
-        for i < 10
-            bugs[i].bugy = bugs[i].bugy + 1
-
-        move down:
-        for i < 10
-            bugs[i].bugy = bugs[i].bugy - 1
-
-
-
-    for movement of the swarm
-        how to change direction if it hit the end
-            get coordinates of dulo right.x, compare to maxX
-            get coordinates of dulo left.x, compare to minX
+void eraseBugs(windowData *game) {
+    for(int number = 0; number < 10; number++) {
+        eraseSingle(game->bugs[number].bugy, game->bugs[number].bugx);
+    }
+}
 
 
-    for killing bugs
-        do every time bullet moves
-        for i < 10
-            if (bugs[i].bugX == game.bulX && bugs[i].bugY == game.bulY)
-                bugs[i].status = 0 DEATH
- */
+//when bug hits ship, kill bug
+
+void eraseSingle(int Y, int X){
+    mvprintw(Y, X, replace);
+}
+
+void eraseShip(int Y, int X){
+    // ship is a 3x2 object
+    mvprintw(Y, X, replace);
+    mvprintw(Y, X+1, replace);
+    mvprintw(Y, X+2, replace);
+    mvprintw(Y+1, X, replace);
+    mvprintw(Y+1, X+1, replace);
+    mvprintw(Y+1, X+2, replace);
+}
+
+void eraseBullet(int Y, int X){
+    // bullet is a 3x3 object
+    mvprintw(Y, X, replace);
+    mvprintw(Y, X+1, replace);
+    mvprintw(Y, X+2, replace);
+    mvprintw(Y+1, X, replace);
+    mvprintw(Y+1, X+1, replace);
+    mvprintw(Y+1, X+2, replace);
+    mvprintw(Y+2, X, replace);
+    mvprintw(Y+2, X+1, replace);
+    mvprintw(Y+2, X+2, replace);
+}
+
+void screenThread(windowData *game){
+    while(true){
+        pthread_mutex_lock(&gameLock);
+        wnoutrefresh(stdscr);
+        doupdate();
+        pthread_mutex_unlock(&gameLock);
+
+        usleep(REFRESH_RATE);
+
+    }
+}
+
+void loseCondition(windowData *game) {
+    for(int i = 0; i < 10; i++) {
+        if((game->bugs[i].status) == ALIVE && (game->bugs[i].bugy >= game->maxY)) {
+            game->winState = LOSE;
+        }
+    }
+}
